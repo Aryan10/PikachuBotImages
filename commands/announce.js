be147103512ce889e2b84358076d822e105afd82
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-03 14:24:35 +0530

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -1,21 +1,23 @@
 const config = require('../config.json');
+
 exports.run = (client, message, args) => {
-if(message.author.id !== config.botOwner) return message.reply('you are not my owner!!!'); 
-let announcemsg = message.content.split(" ").slice(1).join(' ');
-client.guilds.forEach(guild => {
-guild.defaultChannel.send({embed: {
-color:3447003,
-title:`Announcement Message from Bot Owner ${message.author.tag}!`,
-description:announcemsg
-}});
-});
+  if(message.author.id !== config.botOwner) return message.reply('you are not my owner!!!'); 
+  let announcemsg = message.content.split(" ").slice(1).join(' ');
+  client.guilds.forEach(guild => {
+    if (guild.defaultChannel === undefined) return;
+    guild.defaultChannel.send({embed: {
+      color:3447003,
+      title:`Announcement Message from Bot Owner ${message.author.tag}!`,
+      description:announcemsg
+    }});
+  });
 }
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: [],
-  permLevel: 4
+  permLevel: 0
 };
 
 exports.help = {
