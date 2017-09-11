f2655a82c840e1f860993e9a358ca51e41c7f0ba
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-11 09:05:55 +0530

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,6 +1,15 @@
 exports.run = (client, message, args) => {
-let num = message.content.split(" ").slice(1).join(" ");
-message.reply('You rolled :game_die:' + Math.floor(Math.random() * num)).then(m=>m.react('🎲'));
+  let num = message.content.split(" ").slice(1);
+  let [a1, a2] = num.join(" ").replace(/ /g, "").split("-");
+  let str;
+  if (!a2) {
+    [a1, a2] = ["1", a2]
+  }
+  if (!a1) {
+    [a1, a2] = ["1", "100"]
+  }
+  if (!a1.toNumber() || !a2.toNumber()) return message.reply("Please use numbers only.");
+  message.reply('You rolled :game_die:' + Math.floor(Math.random() * a1 + a2)).then(m=>m.react('🎲'));
 };
 
 exports.conf = {
@@ -12,8 +21,8 @@ exports.conf = {
 
 exports.help = {
   name: 'roll',
-  description: 'Rolls a random number from 1 to X.',
-  usage: 'roll [number]',
+  description: 'Rolls a random number from specified numbers. Rolls from 1 to 100 if no number specified.',
+  usage: 'roll number1 - number2',
   module: 'Fun',
   permit: ' ',
   alias: ' '
