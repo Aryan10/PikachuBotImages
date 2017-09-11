6500bdccb970fdd08b4ff0008250368d3a0b83b1
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-11 07:43:32 +0530

    Update pokemon.js

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -1,10 +1,11 @@
 exports.run = (client, message, args) => {
 
 let sprite = message.content.toLowerCase().split(' ').slice(1).join(" ");
+if (!sprite) return;
 if(args[0] === "example"){
 message.channel.send("**Examples**\n`charizard` `pikachu female` `alakazam mega` `charizard megax` `mewtwo megay`\nYou can also use `back/` and `shiny/` before pokemon name to get back and shiny images respectively.\nAdd ` 2` ` 3` ` 4` etc at end of name to get move animations.\n\nNote: Oras Megas, Primals, Oras Forms and Sun/Moon Pokemon are not added yet.");
 }else {
-message.channel.send({file: `http://www.pokestadium.com/sprites/xy/${sprite.replace(/ /g,'-')}.gif`});
+message.channel.send({files: [`http://www.pokestadium.com/sprites/xy/${sprite.replace(/ /g,'-')}.gif`]});
 }
 }
 
