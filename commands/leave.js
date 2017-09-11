b294cade0e0491f02bf4a3e2bcecdaf4b6733a71
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-11 07:29:37 +0530

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -3,6 +3,7 @@ const config = require('../config.json');
 if(message.author.id !== config.botOwner) return message.reply('you are not my owner!!!'); 
 let hyperbeam = message.content.split(" ").slice(1);
 let servername = hyperbeam.join(" ");
+if (!servername) return;
 message.reply('I just left ' + servername);
 client.guilds.find('name', servername).leave()
 }
