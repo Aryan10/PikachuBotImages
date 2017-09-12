e99852383483f11efa1a4fdc35cc68f1d1b02fba
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-12 20:20:17 +0530

    Update emojis.js

--- commands/emojis.js
+++ commands/emojis.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
 
   const guild = message.guild;
-  const embed = new Discord.RichEmbed();
+  const embed = new Discord.RichEmbed()
     .setAuthor(`${guild.name}'s Custom Emojis`, guild.iconURL)
     .setDescription(guild.emojis.map(e=>e.toString()).join(" "));
   message.channel.send({embed});
