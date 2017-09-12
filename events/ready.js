5cab5617f92509c042ad1ca2e5cf597b54440e64
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-12 20:01:52 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,12 +2,6 @@ const pikabot = require("../config.json");
 module.exports = client => {
 
 console.log(`Ready to server in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
-
-if (pikabot.streaming === true) {
-  client.user.setGame(pikabot.game, pikabot.streamingurl);
-  }else {
-  client.user.setGame(pikabot.game)
-  client.user.setStatus(pikabot.status);										    
-}
-
+client.user.setGame(pikabot.game, pikabot.streamingurl);
+client.user.setStatus(pikabot.status);										    
 }
