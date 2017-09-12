e5ae4c33cfc5d0d1e85df23242bf8357f83c119a
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-12 13:44:33 +0530

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -6,11 +6,12 @@ const embed = new Discord.RichEmbed()
 .setColor(3447003)
 .setAuthor(guild.name, guild.iconURL)
 .setThumbnail(guild.iconURL)
+.setFooter(`Type \`${config.prefix}roles\` to get a full list of roles.`)
 .addField("Server ID", guild.id)
 .addField("Server Owner", guild.owner.displayName)
 .addField("Members", `${guild.memberCount} Total | ${guild.members.filter(m=>m.user.bot).size} Bots\n${guild.members.filter(m => m.presence.status === 'online').size} Online | ${guild.members.filter(m => m.presence.status === 'idle').size} Idle | ${guild.members.filter(m => m.presence.status === 'dnd').size} Do Not Disturb`)
 .addField("Channels", `${guild.channels.size} Total\n${guild.channels.filter(c=>c.type === "text").size} Text | ${guild.channels.filter(c=>c.type === "voice").size} Voice\n__Default Channel__: **${guild.defaultChannel.name}**`)
-.addField("Roles", `${guild.roles.size} Total\n_Type \`${config.prefix}roles\` to get a full list of roles._`)
+.addField("Roles", `${guild.roles.size} Total`)
 .addField("Region", guild.region)
 .addField("Created On", guild.createdAt);
 message.channel.send({embed});
