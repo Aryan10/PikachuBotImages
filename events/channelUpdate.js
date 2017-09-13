aaed1d07c38394780531c13de9976e359106a7b4
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-13 18:18:50 +0530

    Create channelUpdate.js

---
+++ events/channelUpdate.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports = (oldch, newch) => {
+  if (oldch.type !== "text" || newch.type !== "text") return;
+  if (oldch.name !== newch.name) {
+    const embed = new Discord.RichEmbed()
+      .setTitle("Channel Name Changed")
+      .addField("Old Name", oldch.name)
+      .addField("New Name", newch.name)
+      .setTimestamp();
+  }else if (oldch.topic !== newch.topic) {
+    const embed = new Discord.RichEmbed()
+      .setTitle("Channel Topic Changed")
+      .addField("Channel", oldch.name)
+      .addField("Old Name", oldch.topic)
+      .addField("New Name", newch.topic)
+      .setTimestamp();
+  }
+  let log = channel.guild.channels.find("name.inclides()", "log");
+  if (!log) {
+    if (!guild.defaultChannel) return;
+    log = guild.defaultChannel;
+  }
+  log.send({embed});
