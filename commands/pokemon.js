56a45c47e2223f96893edc55faa454920cc3caf5
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-14 17:23:57 +0530

    Update pokemon.js

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -2,11 +2,7 @@ exports.run = (client, message, args) => {
 
   let sprite = message.content.toLowerCase().split(' ').slice(1).join(" ");
   if (!sprite) return;
-  if (sprite.isNaN) {
-    message.channel.send({files: [`http://www.pokestadium.com/sprites/xy/${sprite.replace(/ /g,'-')}.gif`]});
-  }else {
-    message.channel.send("Coming soon.");
-  }
+  message.channel.send({files: [`http://www.pokestadium.com/sprites/xy/${sprite.replace(/ /g,'-')}.gif`]});
 }
 
 exports.conf = {
