b04355a7c5c6714305c0fd03835860fd22b46f6b
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-14 22:22:01 +0530

    Update channelUpdate.js

--- events/channelUpdate.js
+++ events/channelUpdate.js
@@ -4,6 +4,11 @@ module.exports = (oldch, newch) => {
   const channel = oldch;
   const guild = oldch.guild;
   let embed;
+  let log = guild.channels.filter(c=>c.name.toLowerCase().includes("log")).first();
+  if (!log) {
+    if (!guild.defaultChannel) return console.log("undefined guild.defaultChannel in " + guild.name);
+    log = guild.defaultChannel;
+  }
   if (oldch.type !== "text" || newch.type !== "text") return;
   if (oldch.name !== newch.name) {
     embed = new Discord.RichEmbed()
@@ -13,21 +18,17 @@ module.exports = (oldch, newch) => {
       .setColor(3447003)
       .setTimestamp()
       .setFooter(" ", guild.iconURL);
+    log.send({embed});
   }
   if (oldch.topic !== newch.topic) {
     embed = new Discord.RichEmbed()
       .setTitle("Channel Topic Changed")
       .addField("Channel", oldch.name)
-      .addField("Old Name", oldch.topic)
-      .addField("New Name", newch.topic)
+      .addField("Old Topic", oldch.topic)
+      .addField("New Topic", newch.topic)
       .setColor(3447003)
       .setTimestamp()
       .setFooter(" ", guild.iconURL);
+    log.send({embed});
   }
-  let log = guild.channels.filter(c=>c.name.toLowerCase().includes("log")).first();
-  if (!log) {
-    if (!guild.defaultChannel) return console.log("undefined guild.defaultChannel in " + guild.name);
-    log = guild.defaultChannel;
-  }
-  log.send({embed});
 };
