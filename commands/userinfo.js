3a3b8f97b117b8c5ef741cbf46c3d7d2f871bb07
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-15 17:09:19 +0530

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,26 +1,35 @@
-exports.run = (client, message, args) => {
-var mention;
-var target;
-if (message.mentions.users.first() !== undefined) {
-mention = message.mentions.users.first();
-target = message.mentions.members.first();
-}else {
-mention = message.author;
-target = message.member;
-}
-
 const Discord = require("discord.js");
-const embed = new Discord.RichEmbed()
-.setAuthor(mention.username, mention.avatatURL)
-.setThumbnail(mention.avatarURL)
-.setColor(4447003)
-.addField("User ID", mention.id)
-.addField("Name", `${target.displayName}`)
-.addField("Joined Server", target.joinedAt)
-.addField("Joined Discord", mention.createdAt)
-.addField("Status", mention.presence.status)
-.addField("Tag", mention.tag)
-.addField(`Roles [${target.roles.size}]`, target.roles.map(r=>r.name).join(", "));
+
+exports.run = (client, message, args) => {
+  let mention = message.mentions.users.first();
+  let target = message.mentions.members.first();
+  if (mention === undefined) {
+    mention = message.author;
+    target = message.member;
+  }
+  let game = mention.presence.game.name;
+  if (!game) {
+    game = "*Nothing*"
+  };
+  let isBot = mention.bot;
+  if (isBot) {
+    isBot = "Yes";
+  }else if (!isBot) {
+    isBot = "No";
+  };
+  
+  const embed = new Discord.RichEmbed()
+    .setAuthor(mention.username, mention.avatatURL)
+    .setThumbnail(mention.avatarURL)
+    .setColor(4447003)
+    .addField("User ID", mention.id)
+    .addField("Name", `${target.displayName}`)
+    .addField("Tag", mention.tag)
+    .addField("Joined Server", target.joinedAt)
+    .addField("Joined Discord", mention.createdAt)
+    .addField("Status", `${mention.presence.status}\n_Playing_ ${game}`)
+    .addField("Bot", isBot)
+    .addField(`Roles [${target.roles.size}]`, target.roles.map(r=>r.name).join(", "));
 message.channel.send({embed});
 };
 
