a897896e9c9c3e737cdd59d05419996763dafcc5
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-17 21:34:15 +0530

    Update prunereactions.js

--- commands/prunereactions.js
+++ commands/prunereactions.js
@@ -1,5 +1,4 @@
 exports.run = (client, message, args) => {
-  if (!message.guild.members.me.hasPermission("MANAGE_MESSAGES")) return message.reply("I don't have the permission to clear reactions from messages.");
   if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have the permission to clear reactions from messages.");
   if (!parseInt(args[0],10)) return message.reply(`Please provide a number of messages to clean reactions from!`);
   message.channel.messages.fetch({limit:parseInt(args[0],10)}).then(msg => {
