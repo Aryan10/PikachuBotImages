0e76df12748126362a93b961a2c1910f5f237efd
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-18 15:05:25 +0530

    Create kick.js

---
+++ commands/kick.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args) => {
+  let member = message.mentions.members.first();
+  let guild = message.guild;
+  if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("You don't have the permission to kick someone.");
+  if (!member) return message.reply('Invalid command usage, you must mention the user to kick someone.');
+  if (!member.kickable) return message.channel.send(`:unamused: You cant kick **${member.user.username}**`);
+  let reason = message.content.split(" ").slice(2).join(" ");
+  if (!reason) reason = "Unknown!";
+  member.send("You have been kicked from **" + guild.name + "**\n\n__Reason__: " + reason);
+  member.kick(reason);
+  // embed //
+  const embed = new Discord.RichEmbed()
+    .setColor(0x595959)
+    .setDescription(`**${member.tag}** is kicked from **${guild.name}** by **${message.author.tag}**.\n__Reason__: ${reason}`)
+    .setFooter("User Kicked")
+    .setTimestamp();
+  // end of embed //
+  message.channel.send({embed});
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['k'],
+  permLevel: 0
+}
+
+exports.help = {
+  name: 'kick', 
+  description: 'Kicks a mentioned user.', 
+  usage: 'kick [mention] <reason>', 
+  module: 'Admin', 
+  permit: 'Requires Kick Member', 
+  alias: '/ k'
+}
