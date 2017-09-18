9588bbc155a6dc723867d5c61b36cb03230cf4b5
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-18 18:34:05 +0530

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
     .setTitle("List Of Servers")
     .setColor(3447003)
     .setDescription(client.guilds.map(g=>g.name).join(", "));
-  if (embed.description > 1998) return message.reply("**Discord API Error**\nCan't send large messages.");
+  if (embed.description.length > 1998) return message.reply("**Discord API Error**\nCan't send large messages.");
   message.channel.send({embed});
 };
 
