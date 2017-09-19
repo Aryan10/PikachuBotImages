bbde97174cf9bcb79a9878e6c5b4aa019cce781b
Aryan10 <aryan.shivam.10@gmail.com>
2017-09-19 19:53:20 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,21 +3,22 @@ const games = require("../util/games.json");
 module.exports = client => {
   console.log(`Ready to server in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
   let streaming = pikabot.streaming;
+  let a = `| ${pikabot.prefix} Prefix | ${client.guilds.size} Servers`
   client.user.setStatus(pikabot.status);
   if (pikabot.rotategame && games.length > 0) {
     setInterval(() => {
       const game = games[~~(Math.random() * games.length)]
       if (!streaming) {
-        client.user.setGame(game)
+        client.user.setGame(game + a)
       }else {
-        client.user.setGame(game, pikabot.streamingurl);
+        client.user.setGame(game + a, pikabot.streamingurl);
       }
     }, pikabot.rotategametime)
   }else {
-    if (streaming === true) {
-      client.user.setGame(`${pikabot.game} | ${client.guilds.size} Servers` , pikabot.streamingurl);
+    if (streaming) {
+      client.user.setGame(`${pikabot.game + a}` , pikabot.streamingurl);
     } else {
-      client.user.setGame(pikabot.game);
+      client.user.setGame(pikabot.game + a);
     }	
   }
 
